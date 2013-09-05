bc81d4164abbdda4eddf7f33e89b12e8cca780e6
David Goehring <dggoeh1@mit.edu>
2013-09-04 23:56:19 -0400

    Create App for Project 0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ps0::Application.config.secret_key_base = 'da3062acbff3fb34ecdc331a414b77d80a6ea0c7375fe481a3111a7fc025ee1c6653e2d7ba94ec600e21b4fb161d6e006fe67367c968566d5ddab89d9892d5ba'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ps0::Application.config.session_store :cookie_store, key: '_ps0_session'
